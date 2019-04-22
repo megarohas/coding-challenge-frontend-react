0166e4cca0f66e41534574236085afd6c74ff977
megarohas <a.galkin@desoft.pro>
2019-04-22 16:25:21 +0600

    typescipt is added

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./App.css";
+
+class App extends React.PureComponent {
+  render() {
+    return <div className="App">Hello Pure World</div>;
+  }
+}
+
+export default App;
