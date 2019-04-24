a6f8e0525765dabf1d59a51bd3c2196f10379cd8
megarohas <a.galkin@desoft.pro>
2019-04-24 16:52:09 +0600

    fetcher is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import styled from "./theme/index";
+import { get } from "./helpers/fetcher.js";
 import {
   BrowserRouter as Router,
   Route,
@@ -56,6 +57,11 @@ const AppRouter = (
 );
 
 class App extends React.PureComponent {
+  componentDidMount() {
+    get(
+      "https://bikewise.org:443/api/v2/incidents?page=1&per_page=10&proximity_square=100"
+    ).then(console.log);
+  }
   render() {
     return (
       <AppWrapper>
---
+++ src/helpers/fetcher.js
@@ -0,0 +1,79 @@
+const CON_TIMEOUT = 999999999;
+
+// function queryParams(params) {
+//   return Object.keys(params)
+//     .map(k => encodeURIComponent(k) + "=" + encodeURIComponent(params[k]))
+//     .join("&");
+// }
+
+function checkStatus(response, resolve, reject) {
+  if (response.status >= 200 && response.status < 300) {
+    response.isError = false;
+    return response;
+  }
+  response.isError = true;
+  return response;
+}
+
+function getResponseJson(response) {
+  return response
+    .json()
+    .then(data => {
+      data.isError = response.isError;
+      return data;
+    })
+    .catch(e => {
+      return e;
+    });
+}
+
+function request(url, method, json) {
+  let body = method === "GET" ? {} : { body: JSON.stringify(json) };
+  return new Promise((resolve, reject) => {
+    const timeout = setTimeout(function() {
+      reject(new Error("Request timed out"));
+    }, CON_TIMEOUT);
+    fetch(url, {
+      ...{
+        method,
+        credentials: "same-origin",
+        mode: "cors",
+        headers: new Headers({
+          "Access-Control-Allow-Origin": "*",
+          Accept: "application/json",
+          "Content-Type": "application/json; charset=UTF-8"
+        })
+      },
+      ...body
+    })
+      .then(response => {
+        clearTimeout(timeout);
+        return response;
+      })
+      .then(response => checkStatus(response, resolve, reject))
+      .then(getResponseJson)
+      .then(data => {
+        const { isError, ...other } = data;
+        if (isError) {
+          reject(other.error);
+        } else resolve(other);
+      })
+      .catch(error => reject(error));
+  });
+}
+
+export function post(url, json) {
+  return request(url, "POST", json);
+}
+
+export function get(url) {
+  return request(url, "GET", {});
+}
+
+export function del(url, json) {
+  return request(url, "DELETE", json);
+}
+
+export function put(url, json) {
+  return request(url, "PUT", json);
+}
