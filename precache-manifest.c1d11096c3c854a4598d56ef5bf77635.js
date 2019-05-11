b81778e4d3608216672a9629caac4d191b6c95c2
Алексей Галкин <megarohas@Aleksejs-MacBook-Air.local>
2019-05-11 18:30:40 +0600

    Updates

---
+++ precache-manifest.c1d11096c3c854a4598d56ef5bf77635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64f13d3b42af5660cb23",
+    "url": "/coding-challenge-frontend-react/static/js/runtime~main.ab36a81e.js"
+  },
+  {
+    "revision": "bec32d76dc3f345b3dc0",
+    "url": "/coding-challenge-frontend-react/static/js/main.110b97f3.chunk.js"
+  },
+  {
+    "revision": "547a9bd898cb3ce35fae",
+    "url": "/coding-challenge-frontend-react/static/js/2.e10e73dc.chunk.js"
+  },
+  {
+    "revision": "bec32d76dc3f345b3dc0",
+    "url": "/coding-challenge-frontend-react/static/css/main.662710fb.chunk.css"
+  },
+  {
+    "revision": "547a9bd898cb3ce35fae",
+    "url": "/coding-challenge-frontend-react/static/css/2.81d15a7e.chunk.css"
+  },
+  {
+    "revision": "7253a56744a4423eb70ce8023ba2308b",
+    "url": "/coding-challenge-frontend-react/index.html"
+  }
+];
