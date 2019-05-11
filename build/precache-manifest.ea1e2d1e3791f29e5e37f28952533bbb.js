78cb059cc38471a713283457c4911cc7e59fa9e6
megarohas <a.galkin@desoft.pro>
2019-05-11 18:15:34 +0600

    fix

---
+++ build/precache-manifest.ea1e2d1e3791f29e5e37f28952533bbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4aadc782f067bb79b911",
+    "url": "/static/js/main.0b63be22.chunk.js"
+  },
+  {
+    "revision": "ef91e1bc71b397ed9a0b",
+    "url": "/static/js/2.8ec6f48c.chunk.js"
+  },
+  {
+    "revision": "4aadc782f067bb79b911",
+    "url": "/static/css/main.662710fb.chunk.css"
+  },
+  {
+    "revision": "ef91e1bc71b397ed9a0b",
+    "url": "/static/css/2.81d15a7e.chunk.css"
+  },
+  {
+    "revision": "5bf703ee4bde14bc09d0c9bddb36c015",
+    "url": "/index.html"
+  }
+];
