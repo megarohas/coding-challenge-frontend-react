4f63c8c17db9f993ac8f83b646c36f137d96059d
megarohas <a.galkin@desoft.pro>
2019-05-11 18:30:00 +0600

    fix

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea1e2d1e3791f29e5e37f28952533bbb.js"
+  "/coding-challenge-frontend-react/precache-manifest.c1d11096c3c854a4598d56ef5bf77635.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/coding-challenge-frontend-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
